83e1478a6e490ce177260253a0bd111f1f347eaf
will.terra <wpterra@outlook.com>
2024-07-22 11:24:19 -0300

    feat: FormEntry component

--- challenge2group1/src/components/HomeComponents/HomeButton.jsx
+++ challenge2group1/src/components/HomeComponents/HomeButton.jsx
@@ -1,6 +1,6 @@
 export default function HomeButton({ text }) {
   return (
-    <button className="flex justify-start bg-lunar text-white py-2 md:py-3 px-10 md:px-14 mt-5 md:mt-10 ml-2 md:ml-0  border-[1px] hover:border-black transition-all">
+    <button className="flex mx-20 bg-bgblack text-white py-2 md:py-3 px-10 md:px-14 mt-5 md:mt-10 ml-2 md:ml-0  border-[1px] hover:border-black transition-all">
       {/* ml-5 mb-5 md:mb-20  */}
       {text}
     </button>
